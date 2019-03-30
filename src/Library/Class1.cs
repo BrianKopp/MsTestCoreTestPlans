f6ff507b9601094f3458e28573fc6e56cb617d6d
Brian Kopp <briankopp.usa@gmail.com>
2019-03-30 14:07:59 -0600

    Moving solution up top

--- src/Library/Class1.cs
+++ src/Library/Class1.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Threading;
 
 namespace Library
 {
     public class Class1
     {
+        public static int Sleep(int milliseconds)
+        {
+            Thread.Sleep(milliseconds);
+            return 0;
+        }
     }
 }
